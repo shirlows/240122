9ddb855fcab2b479c8802927763364919a32093f
Dev Z <devz@gmail.com>
2022-01-24 20:44:18 +0000

    Windows 10 commit

--- a.java
+++ a.java
@@ -3,8 +3,6 @@ This is the intial content for a.java
 void main () {
         println "Hello World"
 	println "hello everyone"
-<<<<<<< HEAD
         println "commit all 3 files"
-=======
->>>>>>> d399d8d15af6a077b577e5494b25674a45627fdf
+        println "Testing windows 10"
 }
